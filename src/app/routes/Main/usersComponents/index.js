88291d688d055c40dafd52c983af126b88d92de7
AbramovNik <abr.nik96@gmail.com>
2019-04-12 20:27:45 +0300

    react app some changes

--- src/app/routes/Main/usersComponents/index.js
+++ src/app/routes/Main/usersComponents/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 // import PropTypes from "prop-types";
 import "./styles.scss";
+import "../../../../utils/images/icomoon/fonts/icomoon.svg"
 import {Col} from "reactstrap"
 const users = [];
 
@@ -8,11 +9,22 @@ export class UserItem extends React.Component {
 render(){
     const {user} = this.props;
     return (
-        <Col xs = "12" sm = "6" md = "4" lg = "3">
-        <div className = "user-info">
-            <img src = {user.avatar_url}/>
-            <h2>{`User login - ${user.login}`}</h2>
-         </div>
+        <Col  xs = "12" sm = "6" md = "4" lg = "3">
+            <div className = "user-hold">
+                <div className = "image-hold">
+                    <img src = {user.avatar_url}/>
+                </div>
+                <div className = "user-info">
+                    <strong className = "user-name">{`User login - ${user.login}`}</strong>
+                </div>
+                <div className = "hover-info">
+                        <ul className = "social-media">
+                            <li><a href="#"><span className="icon-facebook"></span></a></li>
+                            <li><a href="#">Twitter</a></li>
+                            <li><a href="#">LinkedIn</a></li>
+                        </ul>
+                    </div>
+            </div>
          </Col>
     )
 }
